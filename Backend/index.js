23d86b6831dfc0aea332e224de5196d6b405e92e
Joaotrein <Jpgtrein@gmail.com>
2024-05-22 17:56:54 -0300

    Hotfix - CORS AllowedHeaders

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: ["*"],
+        origin: "*",
         methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
         allowedHeaders: 'Content-Type,Authorization',
     })
